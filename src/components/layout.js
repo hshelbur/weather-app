e48ce9ea46a38264d51b782a5e616d2f36893e3c
Howard Shelburne <howard.shelburne@gmail.com>
2019-05-25 15:17:37 -0400

    a little bit of styling, fix for navigator being undefined in webpack

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,7 +36,7 @@ const Layout = ({ children }) => (
         >
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
+            © {new Date().getFullYear()}, Built by <a href="https://github.com/hshelbur">Howard Shelburne</a> with
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import {useFormState} from 'react-use-form-state'
 
 import Layout from "../components/layout"
+import "./index.css"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
@@ -12,7 +13,6 @@ const IndexPage = () => {
   const [zipCode, setZipCode] = useState()
   const [coordinates, coordinatesForm] = useFormState()
   const [currentPosition, setCurrentPosition] = useState({})
-  const [recentSearches, setRecentSearches] = useState([])
 
   function locationSuccess(position) {
     const latitude  = position.coords.latitude
@@ -24,14 +24,13 @@ const IndexPage = () => {
     alert('Unable to retrieve your location')
   }
 
-  if (!navigator.geolocation) {
-    alert('Geolocation is not supported by your browser')
-  } else {
-    navigator.geolocation.getCurrentPosition(locationSuccess, locationError)
+  if (typeof navigator !== `undefined`) {
+    if (!navigator.geolocation) {
+      alert('Geolocation is not supported by your browser')
+    } else {
+      navigator.geolocation.getCurrentPosition(locationSuccess, locationError)
+    }
   }
-
-  console.log(coordinates)
-  console.log(`new query is ${newQuery}`)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -91,19 +90,19 @@ const IndexPage = () => {
       <div className="query">
         <label htmlFor="city">City: </label>
         <input type="text" onBlur={(e) => setCity(e.target.value)} />
-        <button onClick={(e) => {e.preventDefault(); setNewQuery(`city`)}}>Submit</button>
+        <button className="btn" onClick={(e) => {e.preventDefault(); setNewQuery(`city`)}}>Submit</button>
       </div>
       <div className="query">
         <label htmlFor="zip">Zip Code: </label>
         <input type="text" onBlur={(e) => setZipCode(e.target.value)} />
-        <button onClick={(e) => {e.preventDefault(); setNewQuery(`zip`)}}>Submit</button>
+        <button className="btn" onClick={(e) => {e.preventDefault(); setNewQuery(`zip`)}}>Submit</button>
       </div>
       <form className="query coordinates-form">
         <label htmlFor="latitude">Latitude: </label>
         <input required {...coordinatesForm.number('latitude')} />
         <label htmlFor="longitude">Longitude: </label>
         <input required {...coordinatesForm.number('longitude')} />
-        <button onClick={(e) => { e.preventDefault(); setNewQuery(`coordinates`)}}>Submit</button>
+        <button className="btn" onClick={(e) => { e.preventDefault(); setNewQuery(`coordinates`)}}>Submit</button>
       </form>
       {data.main && 
         <div className="weather-data">
